81ab2fe569de1f97fa2186e2078ced91086acaa0
Paul Marquardt <marqrdt@gmail.com>
2023-08-03 19:57:18 -0600

    Final changes to Part 1.

--- django-social/dwitter/models.py
+++ django-social/dwitter/models.py
@@ -2,6 +2,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -13,9 +14,7 @@ class Profile(models.Model):
     )
     def __str__(self):
         return self.user.username
-
-from django.db.models.signals import post_save
-
+    
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
     print("Running create_profile callback!")
